3abdb16d67469fefaf8d45227767080c11f7be64
Emetsu <valmores.russel@gmail.com>
2022-11-20 07:35:21 +0800

    added navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,9 @@
-<script>import "../app.postcss";</script><slot></slot>
+<script>import "../app.postcss";</script>
+
+<nav class="bg-blue-200 flex gap-11">
+    <a class="text-red-600" href="/">Homepage</a>
+    <a class="text-green-600" href="/">About page</a>
+</nav>
+
+
+<slot />
